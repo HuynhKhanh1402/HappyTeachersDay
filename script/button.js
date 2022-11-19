ea4c8d846b83808de84dece48d6d4ae374c44685
KhanhHuynh1402 <khanh0787722636@gmail.com>
2022-11-20 00:16:41 +0700

    Update

--- script/button.js
+++ script/button.js
@@ -1,6 +1,6 @@
 var button = document.getElementById('playBut')
 button.onclick = function() {
-    var audio = new Audio("https://github.com/HuynhKhanh1402/HappyTeachersDay/blob/main/resources/sound.mp3?raw=true")
+    var audio = new Audio("resources/sound.mp3")
     audio.loop = true
     audio.play()
     document.getElementById("button").remove()
